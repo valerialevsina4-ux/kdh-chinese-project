0b3d6308bab56a378b66c0bdca0f48081cab2ee5
Valeria Levshina <valerialevsina4@gmail.com>
2026-07-04 18:52:48 +0700

    Update Study Cards Script

--- study-cards.js
+++ study-cards.js
@@ -17,3 +17,55 @@ startButton.addEventListener("click", function(){
 
 const closeOverlay =
 document.querySelector(".close-overlay");
+
+closeOverlay.addEventListener("click", function(){
+
+    deckOverlay.style.display = "none";
+});
+
+startLearning.addEventListener("click", function(){
+
+    deckOverlay.style.display = "none";
+
+    flashcardOverlay.style.display = "flex";
+});
+
+const flashcard =
+document.querySelector(".flashcard");
+
+flashcard.addEventListener("click", function(){
+
+    flashcard.classList.toggle("flipped");
+});
+
+const cards = [
+
+    {
+        chinese:"老虎",
+
+        pinyin:"lǎohǔ",
+
+        english:"Tiger",
+
+        image:"Tiger.jpg"
+    },
+]
+
+let currentCard =
+Math.floor(Math.random()*cards.length);
+
+function showCard(index){
+
+}
+
+function nextCard(){
+
+}
+
+currentCard++;
+
+showCard(currentCard);
+
+function updateProgress(){
+    
+}

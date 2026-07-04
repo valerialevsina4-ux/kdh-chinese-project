2a96860e77d0be75a994169bbdf0c856d327a45a
Valeria Levshina <valerialevsina4@gmail.com>
2026-07-04 17:26:38 +0700

    Update JavaScript

--- study-cards.js
+++ study-cards.js
@@ -1,1 +1,19 @@
-console.log("Study Cards Loaded!");
+const startButton =
+document.querySelector(".start-button");
+
+const deckOverlay =
+document.querySelector(".deck-overlay");
+
+const flashcardOverlay =
+document.querySelector(".flashcard-overlay");
+
+const startLearning =
+document.querySelector(".start-learning");
+
+startButton.addEventListener("click", function(){
+
+    deckOverlay.style.display = "flex";
+});
+
+const closeOverlay =
+document.querySelector(".close-overlay");
